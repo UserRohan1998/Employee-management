7061e34e7fe847044b1e210a7a805cfe6ca81f44
Rohan <rohan98das@gmail.com>
2024-05-12 09:32:17 +0100

    new fikes
    Addded rest of it

---
+++ src/main/java/com/regp/RegistrationProjApplication.java
@@ -0,0 +1,13 @@
+package com.regp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regp/RegistrationProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
